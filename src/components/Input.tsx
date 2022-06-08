4d064344e6b5d7e5d23468e5b45a236f86f39702
Dominik Rettig <drettig@apptension.com>
2022-06-08 10:17:58 +0200

    fix error

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,26 +9,27 @@ const Input = () => {
     const dispatch = useDispatch()
 
     const addTodo = () => {
-        if(inputValue.length !== 0){
-        dispatch(saveTodo({
-            item: inputValue,
-            done: false,
-            id: Date.now()
-        }))
-        setInput('')} else {
+        if (inputValue.length !== 0) {
+            dispatch(saveTodo({
+                item: inputValue,
+                done: false,
+                id: Date.now()
+            }))
+            setError(false)
+            setInput('')
+        } else {
             setError(true)
         }
     }
 
     return (
         <>
-        <div className="input">
-            <input type="text" value={inputValue} onChange={(e) => setInput(e.target.value)} />
-            <button onClick={addTodo} >Add</button>
-            
-        </div>
-        {error && <p className="error">
-            Enter the name of the task
+            <div className="input">
+                <input type="text" value={inputValue} onChange={(e) => setInput(e.target.value)} />
+                <button onClick={addTodo} >Add</button>
+            </div>
+            {error && <p className="error">
+                Enter the name of the task
             </p>}
         </>
     )
